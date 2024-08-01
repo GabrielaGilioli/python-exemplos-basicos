2743513feaddd21ee519f913c6e0ed0b768ef9c2
GabrielaGilioli <gabrielagilioli.senai@gmail.com>
2024-08-01 10:46:25 -0300

    .
    .

---
+++ 02_variaveis.py
@@ -0,0 +1,17 @@
+#Variáveis
+nome = "Loana Andrade"
+idade = 17
+
+#Exibição de valores
+print(nome)
+print(idade)
+print("\n")
+
+# 1º método de concatenação (string: str)
+print("O meu nome é: " + nome + " e tenho " + str(idade) + " anos.\n")
+
+# 2º Método de concatenação (format)
+print("O meu nome é {} e tenho {} anos.\n".format(nome, idade))
+
+# 3º de concatenação (f string)
+print(f"O meu nome é: {nome} e tenho {idade} anos.")
