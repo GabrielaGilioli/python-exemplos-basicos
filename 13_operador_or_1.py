d8816e38714da2400172d926e0cdf6a8ac24fa17
GabrielaGilioli <gabrielagilioli.senai@gmail.com>
2024-08-01 15:40:22 -0300

    .
    .

---
+++ 13_operador_or_1.py
@@ -0,0 +1,8 @@
+idade_kaue = 17
+idade_mateus = 15
+
+# Operador OR (Ou)
+if idade_kaue >=18 or idade_mateus >=18:
+    print("Pelo menos um dos dois é maior de idade.")
+else:
+    print("Kaue e Mateus não são maiores de idade")
---
+++ 14_operador_and.py
@@ -0,0 +1,9 @@
+idade_kaue = 17
+idade_mateus = 19
+
+# Operador And (E)
+if idade_kaue >=18 and idade_mateus >=18:
+    print("Pelo menos um dos dois é maior de idade.")
+else:
+    # os 2 devem ser menores de idade
+    print("Kaue e Mateus não são maiores de idade")
